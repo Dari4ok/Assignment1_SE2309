b548d1e49739427c0a5918bc7dd80b40baa1336e
dari4ok <darkhanb05@gmail.com>
2023-12-22 15:31:18 +0600

    Creating classes

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import action.Shape;
+import action.Point;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Q:\\oop_java\\Assignment1se2309\\src\\source");
+        Scanner sc = new Scanner(file);
+        int i = 0;
+        while (sc.hasNext()){
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            Point point = new Point(x, y);
+            shape.addPoint(point);
+        }
+    }
+}
---
+++ src/action/Shape.java
@@ -0,0 +1,14 @@
+package action;
+
+import action.Point;
+
+import java.util.ArrayList;
+
+public class Shape {
+    private ArrayList<Point> points;
+
+    public void addPoint(){
+        Point.getDistance();
+    }
+
+}
